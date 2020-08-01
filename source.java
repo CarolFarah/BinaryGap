cef75c38103672dab47f563af142d0544143a52a
Ana Carolina Farah <anacarolfarah@gmail.com>
2020-08-01 02:31:03 -0300

    Update source.java

--- source.java
+++ source.java
@@ -1,25 +1,29 @@
 // you can also use imports, for example:
 // import java.util.*;
+
 // you can write to stdout for debugging purposes, e.g.
 // System.out.println("this is a debug message");
+
 class Solution {
-public int solution(int N) {
-int maxGap = 0;
+     public int solution(int N) {
+       int maxGap = 0;
+       
 // remove trailing zeroes if not counted; credit to Peter Taylor
-while (N % 2 == 0) {
-N /= 2;
-}
-for ( int i = 0; N > 0; N /= 2) {
-if (N % 2 == 0) {
-i++;
-}
-else {
-if ( i > maxGap) {
-maxGap = i;
-}
-i = 0;
-}
-}
-return maxGap;
-}
+       while (N % 2 == 0) {
+             N /= 2;
+       }
+             for ( int i = 0; N > 0; N /= 2) {
+                 if (N % 2 == 0) {
+                     i++;
+                }
+                else {
+                     if ( i > maxGap) {
+                          maxGap = i;
+                    }
+                    i = 0;
+              }
+               
+              }
+                return maxGap;
+          }
 }
